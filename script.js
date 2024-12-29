140470cc7d3eb7f8c50663be30e5a0326b1f5361
epaisaroz <epaisaroz@gmail.com>
2024-12-29 20:44:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -111,3 +111,21 @@ window.addEventListener("resize", function () {
 
   moveSliderItem();
 });
+
+
+
+
+
+// alert msg
+
+  const popup = document.querySelector('.popup');
+        const x = document.querySelector('.popup-content h1')
+
+        window.addEventListener('load', () => {
+            popup.classList.add('showPopup');
+            popup.childNodes[1].classList.add('showPopup');
+        })
+        x.addEventListener('click', () => {
+            popup.classList.remove('showPopup');
+            popup.childNodes[1].classList.remove('showPopup');
+        })
